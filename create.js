1a7daf41f36755e8913c40a34c89500931243e05
Amo Moloko <u14284783@tuks.co.za>
2018-08-08 17:29:00 +0200

    create

---
+++ create.js
@@ -0,0 +1,33 @@
+//create claim
+
+import uuid from "uuid";
+import AWS from "aws-sdk";
+import { success, failure } from "./libs/response-lib";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+
+
+export async function main(event, context, callback){
+    const data = JSON.parse(event.body);
+
+    const params = {
+        TableName: process.env.tableName,
+        Item: {
+            userId: event.requestContext.identity.cognitoIdentity,
+            claimId: uuid.v1(),
+            content: data.content,
+            claimNumber: data.claimNumber,
+            email: data.email,
+            attachment: data.attachment,
+            incidentDate: data.incidentDate,
+            createdAt: Date.now()
+        }
+    };
+
+    try {
+        await dynamoDbLib.call("put", params);
+        callback(null, success(params.Item));
+    } catch (e){
+        console.log(e);
+        callback(null, failure({status: false}));
+    }
+}
